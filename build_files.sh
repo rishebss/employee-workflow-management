636f876a2aa7a88b1279ba0fc52844d465506777
Risheb.s <rishebs123456@gmail.com>
2025-12-18 23:23:11 +0530

    first commit

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Install dependencies (vercel may already install, but ensure collectstatic can run)
-python -m pip install -r requirements.txt
-# Collect static files to the directory expected by vercel.json ("static")
-python manage.py collectstatic --noinput
